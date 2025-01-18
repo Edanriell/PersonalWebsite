1e6261e99c52cf6c153cdcdbac3a05c3a9e7e483
Edanriell <Edanriell@gmail.com>
2025-01-18 21:36:17 +0200

    feature(clouds-background): created clouds background

--- Client/src/next-pages/home/ui/home-page.tsx
+++ Client/src/next-pages/home/ui/home-page.tsx
@@ -1,5 +1,14 @@
 import { type FC } from "react";
 
 export const HomePage: FC = () => {
-	return <h1>Edanriell</h1>;
+	return (
+		<main className="relative">
+			<section className="flex flex-col items-center justify-center min-h-[100vh]">
+				<h1 className="font-medium text-[42rem] text-amber-50">
+					Edanriell
+					<span className="sr-only">Front-End Engineer and Tech Enthusiast</span>
+				</h1>
+			</section>
+		</main>
+	);
 };
