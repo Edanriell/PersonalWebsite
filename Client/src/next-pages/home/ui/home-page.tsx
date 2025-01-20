e50f4c86766e78f674229c9c9effdff19003dc8b
Edanriell <Edanriell@gmail.com>
2025-01-20 10:36:55 +0200

    feature(home-page): created home page

--- Client/src/next-pages/home/ui/home-page.tsx
+++ Client/src/next-pages/home/ui/home-page.tsx
@@ -4,10 +4,11 @@ export const HomePage: FC = () => {
 	return (
 		<main className="relative">
 			<section className="flex flex-col items-center justify-center min-h-[100vh]">
-				<h1 className="font-medium text-[42rem] text-amber-50">
+				<h1 className="font-bold text-[56rem] text-white drop-shadow-lg">
 					Edanriell
 					<span className="sr-only">Front-End Engineer and Tech Enthusiast</span>
 				</h1>
+				<button type="button">Send message</button>
 			</section>
 		</main>
 	);
