5c6fb802d2caf48e2de3a8eae4f3a8294efdabd2
Edanriell <Edanriell@gmail.com>
2025-01-20 22:43:15 +0200

    feature(home-page-refactor): minor refactors

--- Client/src/next-pages/home/ui/home-page.tsx
+++ Client/src/next-pages/home/ui/home-page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@shared/ui/button";
 export const HomePage: FC = () => {
 	return (
 		<main className="relative">
-			<section className="flex flex-col items-center justify-center min-h-[100vh]">
+			<section className="flex flex-col items-center justify-center min-h-[100vh] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-8%]">
 				<h1 className="font-bold text-[52rem] md:text-[56rem] text-white drop-shadow-lg">
 					Edanriell
 					<span className="sr-only">Front-End Engineer and Tech Enthusiast</span>
