515631339d40c9b07d73401eef068fa8da1f0d3b
Edanriell <Edanriell@gmail.com>
2025-01-21 12:09:15 +0200

    feature(morph-title): working on morph title

---
+++ Client/src/widgets/morph-title/ui/morph-title.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { type FC } from "react";
+import { AnimatePresence, motion } from "motion/react";
+
+export const MorphTitle: FC = ({ children }: { children: string | string[] }) => {
+	const generateKeys = (text: string) => {
+		const charCount: { [key: string]: number } = {};
+
+		return text.split("").map((char) => {
+			if (!charCount[char]) {
+				charCount[char] = 0;
+			}
+
+			const key = `${char}-${charCount[char]}`;
+			charCount[char]++;
+
+			return { char, key };
+		});
+	};
+
+	const textToDisplay = generateKeys(children as string);
+
+	return (
+		<AnimatePresence mode="popLayout" initial={false}>
+			{textToDisplay.map(({ char, key }) => (
+				<motion.span
+					key={key}
+					layoutId={key}
+					className="inline-block text-inherit"
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{
+						duration: 0.25,
+						type: "spring",
+						bounce: 0,
+						opacity: {
+							duration: 0.35,
+							type: "spring",
+							bounce: 0
+						}
+					}}
+				>
+					{char === " " ? "\u00A0" : char}
+				</motion.span>
+			))}
+		</AnimatePresence>
+	);
+};
